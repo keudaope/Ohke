1de74b8591bebdddff37df644106850e0dd891ce
Jyri Lindroos <jyri.lindroos@keuda.fi>
2023-10-05 13:51:25 +0300

    JavaScript koodia Lumoakatemiaan

--- Projekti01/lumoakatemia.js
+++ Projekti01/lumoakatemia.js
@@ -179,3 +179,168 @@ function karkaus(vuosi) {
 karkaus(2000);
 karkaus(2100);
 karkaus(1997);*/
+
+// Tee ohjelma, joka tarkastaa, onko annettu
+// luku parillinen vai pariton
+function parillinenPariton(luku) {
+    if (luku % 2 == 0) {
+        console.log("Luku on parillinen");
+    }
+    else {
+        console.log("Luku on pariton");
+    }
+}
+/*parillinenPariton(3);
+parillinenPariton(6);
+parillinenPariton(0);*/
+
+// Tee ohjelma, joka tarkistaa,
+// onko annettu merkki kirjain vai ei
+function kirjainko(merkki) {
+    if (merkki.length == 1 && merkki.match(/[a-öA-Ö]/i)) {
+        console.log("Annettu merkki on kirjain");
+    }
+    else {
+        console.log("Annettu merkki ei ole kirjain");
+    }
+}
+/*kirjainko("g");
+kirjainko(1);
+kirjainko("!");
+kirjainko("ä");
+kirjainko("H");
+kirjainko("Ö");*/
+
+// Tee ohjelma, joka tarkastaa, onko annettu
+// merkki vokaali vai konsonantti
+function vokaaliKonsonantti(merkki) {
+    if (merkki.match(/[aeiouyäö]/i)) {
+        console.log("Annettu merkki on vokaali");
+    }
+    else if (merkki.match(/[bcdfghjklmnpqrstvwxz]/i)) {
+        console.log("Annettu merkki on konsonantti");
+    }
+    else {
+        console.log("Annettu merkki ei ole kumpikaan");
+    }
+}
+/*vokaaliKonsonantti("C");
+vokaaliKonsonantti("e");
+vokaaliKonsonantti("?");*/
+
+// Tee ohjelma, joka tarkastaa, onko annettu
+// merkki kirjain, numero vai erikoismerkki
+function kiNuEm(merkki) {
+    let kirjain = /[a-öA-Ö]/i;
+    let numero = /[0-9]/i;
+    if (merkki.length == 1 && merkki.match(kirjain))
+    {
+        console.log("Merkki on kirjain");
+    }
+    else if (merkki.match(numero))
+    {
+        console.log("Merkki on numero");
+    }
+    else
+    {
+        console.log("Merkki on erikoismerkki");
+    }
+}
+/*kiNuEm("A");
+kiNuEm("6");
+kiNuEm("/");*/
+
+// Tee ohjelma, joka tarkastaa,
+// onko annettu kirjain iso vai pieni
+function isoPieni(merkki) {
+    if (merkki.match(/[a-öA-Ö]/i) && merkki == merkki.toUpperCase()) {
+        console.log("Merkki on iso kirjain");
+    }
+    else if (merkki.match(/[a-öA-Ö]/i) && merkki == merkki.toLowerCase()) {
+        console.log("Merkki on pieni kirjain");
+    }
+    else {
+        console.log("Merkki ei ole kirjain");
+    }
+}
+/*isoPieni("v");
+isoPieni("T");
+isoPieni("?");*/
+
+//  Tee ohjelma, joka laskee annetuista
+// myynti ja ostoluvuista, saitko voittoa
+// vai tappiota
+function voittoTappio(myynti, osto) {
+    myynti = parseInt(myynti);
+    osto = parseInt(osto);
+    if ((myynti - osto) > 0) {
+        console.log("Sait voittoa: " + (myynti - osto));
+    }
+    else {
+        console.log("Tappiosi oli " + (myynti - osto));
+    }
+}
+/*voittoTappio(30, 20);
+voittoTappio(20, 30);
+voittoTappio(30, 30);*/
+
+function koePisteet(Ma, KeFy, Ai, Ru, En) {
+    var taulukko = [["Matematiikka", Ma], ["KemiaFysiikka", KeFy], ["Aidinkieli", Ai], ["Ruotsi", Ru], ["Englanti", En]];
+    for (var i = 0; i < taulukko.length; i++) {
+        if (taulukko[i][1] >= 90) {
+            console.log(taulukko[i][0] + ": K5");
+        }
+        else if (taulukko[i][1] >= 80) {
+            console.log(taulukko[i][0] + ": H4");
+        }
+        else if (taulukko[i][1] >= 70) {
+            console.log(taulukko[i][0] + ": H3");
+        }
+        else if (taulukko[i][1] >= 60) {
+            console.log(taulukko[i][0] + ": T2");
+        }
+        else if (taulukko[i][1] >= 50) {
+            console.log(taulukko[i][0] + ": T1");
+        }
+        else {
+            console.log(taulukko[i][0] + ": Hylätty");
+        }
+    }
+}
+/*koePisteet(55, 78, 63, 88, 94);
+koePisteet(99, 45, 70, 80, 60);*/
+// Tee ohjelma, joka tulostaa viikonpäivät, 
+//kun annetaan vkpnumero
+function viikonpaiva(nro) {
+    nro = parseInt(nro);
+    console.log(nro);
+    switch (nro) {
+        case 1:
+            console.log("Maanantai");
+            break;
+        case 2:
+            console.log("Tiistai");
+            break;
+        case 3:
+            console.log("Keskiviikko");
+            break;
+        case 4:
+            console.log("Torstai");
+            break;
+        case 5:
+            console.log("Perjantai");
+            break;
+        case 6:
+            console.log("Lauantai");
+            break;
+        case 7:
+            console.log("Sunnuntai");
+            break;
+        default:
+            console.log("Anna numero välillä 1-7");
+            break;
+    }
+}
+/*viikonpaiva(5);
+viikonpaiva(0);
+viikonpaiva(8);*/
